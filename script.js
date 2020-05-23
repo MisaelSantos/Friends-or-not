c2f08540480010ab134bee8363910f77218e6da2
MisaelSantos <misaelsan2017@outlook.com>
2020-05-23 19:44:45 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const number = document.querySelector('div div form input');
+const subject = document.querySelector('div div form textarea');
+const btn = document.querySelector('div div form button');
+
+btn.onclick = () => {
+
+}
