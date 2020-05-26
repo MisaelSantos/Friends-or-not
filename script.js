2a28f468e6e8e4a078ac71e54dd5e7db7a18ca90
MisaelSantos <misaelsan2017@outlook.com>
2020-05-25 21:51:08 -0300

    add formaction

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const number = document.querySelector('div div form input');
 const subject = document.querySelector('div div form textarea');
 const btn = document.querySelector('div div form button');
-
-btn.onclick = () => {
-
-}
+btn.setAttribute('formaction', 'https://www.google.com.br');
